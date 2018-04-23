39ee01d8412c3b362dc64193e2ad7d04eccefa97
William Getz <willegetz@gmail.com>
2018-04-23 08:42:02 -0700

    Initialization of test file and adding eslint

---
+++ test/blah.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { assert } = require('chai');
+
+describe('blah description', function () {
+    it('will fail because of blah', function () {
+        assert.equal(true, false);
+    });
+});
