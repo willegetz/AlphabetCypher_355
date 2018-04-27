0e1850f9a6aea82970b21a9c9575f8560592a5c7
William Getz <willegetz@gmail.com>
2018-04-27 07:56:19 -0700

    Refactoring

--- app/alphabetCypher.js
+++ app/alphabetCypher.js
@@ -1,49 +1,44 @@
 'use strict';
 
-function alphabetCypher(){
-    function performDecryption(messageToDecrypt, passKey){
-        const alphabetMap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function alphabetCypher() {
+    const alphabetMap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    function getDecryptedLetterIndex(keyLetterIndex, letterToDecryptIndex) {
+        return ((alphabetMap.length - keyLetterIndex) + letterToDecryptIndex) % alphabetMap.length;
+    }
+
+    function getEncryptedLetterIndex(keyLetterIndex, letterToEncryptIndex) {
+        return (keyLetterIndex + letterToEncryptIndex) % alphabetMap.length;
+    }
+
+    function applyCypher(messageToDecrypt, passKey, decryptionOperation) {
         let decryptedMessage = '';
-    
+
         for (var i = 0; i < messageToDecrypt.length; i++) {
             const keyIndex = i % passKey.length;
             const keyLetter = passKey[keyIndex];
-    
-            const letterToDecrypt = messageToDecrypt[i];
-            
-            const keyLetterIndex = alphabetMap.indexOf(keyLetter.toUpperCase());
 
+            const letterToDecrypt = messageToDecrypt[i];
+
+            const keyLetterIndex = alphabetMap.indexOf(keyLetter.toUpperCase());
             const letterToDecryptIndex = alphabetMap.indexOf(letterToDecrypt.toUpperCase());
-            const decryptedLetterIndex = ((alphabetMap.length - keyLetterIndex) + letterToDecryptIndex) % alphabetMap.length;
+
+            const decryptedLetterIndex = decryptionOperation(keyLetterIndex, letterToDecryptIndex);
 
             const decryptedLetter = alphabetMap[decryptedLetterIndex];
 
             decryptedMessage += decryptedLetter;
         }
-    
+
         return decryptedMessage;
     }
 
-    function performEncryption(messageToEncrypt, passKey){
-        const alphabetMap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        let encryptedMessage = '';
-    
-        for (var i = 0; i < messageToEncrypt.length; i++) {
-            const keyIndex = i % passKey.length;
-            const keyLetter = passKey[keyIndex];
-    
-            const letterToEncrypt = messageToEncrypt[i];
-    
-            const keyLetterIndex = alphabetMap.indexOf(keyLetter.toUpperCase());
-            const letterToEncryptIndex = alphabetMap.indexOf(letterToEncrypt.toUpperCase());
-    
-            const encryptedLetterIndex = ((keyLetterIndex + letterToEncryptIndex) % alphabetMap.length);
-            const encryptedLetter = alphabetMap[encryptedLetterIndex];
-    
-            encryptedMessage += encryptedLetter;
-        }
-    
-        return encryptedMessage;
+    function performDecryption(messageToDecrypt, passKey) {
+        return applyCypher(messageToDecrypt, passKey, getDecryptedLetterIndex);
+    }
+
+    function performEncryption(messageToEncrypt, passKey) {
+        return applyCypher(messageToEncrypt, passKey, getEncryptedLetterIndex);
     }
 
     return {
